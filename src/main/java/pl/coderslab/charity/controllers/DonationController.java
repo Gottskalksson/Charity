06faada216bd8e28717e78e4428a7b70c3771cf3
Gottskalksson <adam.rolirad@gmail.com>
2020-06-29 12:27:18 +0200

    added methods to control form

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -1,18 +1,47 @@
 package pl.coderslab.charity.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.entity.Donation;
+import pl.coderslab.charity.entity.Institution;
+import pl.coderslab.charity.repositories.CategoryRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/donations", produces = "text/plain;charset=utf-8")
 public class DonationController {
 
+    private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
+
+    @Autowired
+    public DonationController(final CategoryRepository categoryRepository, final InstitutionRepository institutionRepository) {
+        this.categoryRepository = categoryRepository;
+        this.institutionRepository = institutionRepository;
+    }
+
     @GetMapping("/add")
     public String viewForm (Model model) {
+        List<Category> categoryList = categoryRepository.findAll();
+        model.addAttribute("categoryList", categoryList);
+
+        List<Institution> institutionList = institutionRepository.findAll();
+        model.addAttribute("institutionList", institutionList);
+
         model.addAttribute("donation", new Donation());
+
         return "form";
+    }
+
+    @PostMapping("/add")
+    public String formConfirmation () {
+        return "form-confirmation";
     }
 }
