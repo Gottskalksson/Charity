7756d23dbf4a878f23ee373999a5745bdb0f2862
Gottskalksson <adam.rolirad@gmail.com>
2020-07-01 01:22:07 +0200

    finished js file, where summary form

--- src/main/webapp/resources/js/summaryApp.js
+++ src/main/webapp/resources/js/summaryApp.js
@@ -5,7 +5,6 @@ $(function () {
         var bagsQuantity = $("#quantity").val();
         $("#summaryBag").text("sztuk: " + bagsQuantity);
 
-        console.log(bagsQuantity);
 
     });
 
@@ -14,8 +13,36 @@ $(function () {
         var radioValue = $("input[name='institution']:checked").siblings().children("div.title").text();
 
         $("#summaryInstitution").text("dla fundacji " + radioValue);
-        console.log(radioValue);
 
+
+    });
+
+    $(".detailsNextStep").click(function () {
+
+        var street = $("#street").val();
+        $("#summaryStreet").text(street);
+
+        var city = $("#city").val();
+        $("#summaryCity").text(city);
+
+        var zipCode = $("#zipCode").val();
+        $("#summaryZipCode").text(zipCode);
+
+        var phoneNumber = $("#phoneNumber").val();
+        $("#summaryPhone").text(phoneNumber);
+
+        var pickUpDate = $("#pickUpDate").val();
+        $("#summaryPickUpDate").text(pickUpDate);
+
+        var pickUpTime = $("#pickUpTime").val();
+        $("#summaryPickUpTime").text(pickUpTime);
+
+        var comments = $("#pickUpComment").val();
+        if (comments.length === 0) {
+            $("#summaryCommentary").text("Brak uwag");
+        } else {
+            $("#summaryCommentary").text(comments);
+        }
     });
 
 
