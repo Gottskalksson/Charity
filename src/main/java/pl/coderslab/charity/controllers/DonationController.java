5ea49bdaad60e7d88a40be03e47dcd054c99e1eb
Gottskalksson <adam.rolirad@gmail.com>
2020-07-02 01:04:45 +0200

    Finished post method to save object in database

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -3,15 +3,22 @@ package pl.coderslab.charity.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.entity.Category;
 import pl.coderslab.charity.entity.Donation;
 import pl.coderslab.charity.entity.Institution;
 import pl.coderslab.charity.repositories.CategoryRepository;
+import pl.coderslab.charity.repositories.DonationRepository;
 import pl.coderslab.charity.repositories.InstitutionRepository;
 
+import javax.validation.Valid;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Controller
@@ -20,11 +27,14 @@ public class DonationController {
 
     private final CategoryRepository categoryRepository;
     private final InstitutionRepository institutionRepository;
+    private final DonationRepository donationRepository;
 
     @Autowired
-    public DonationController(final CategoryRepository categoryRepository, final InstitutionRepository institutionRepository) {
+    public DonationController(final CategoryRepository categoryRepository, final InstitutionRepository institutionRepository,
+                              final DonationRepository donationRepository) {
         this.categoryRepository = categoryRepository;
         this.institutionRepository = institutionRepository;
+        this.donationRepository = donationRepository;
     }
 
     @GetMapping("/add")
@@ -35,13 +45,25 @@ public class DonationController {
         List<Institution> institutionList = institutionRepository.findAll();
         model.addAttribute("institutionList", institutionList);
 
+        LocalDate date = LocalDate.now();
+        model.addAttribute("nowDate", date);
+
+        LocalTime time = LocalTime.now().truncatedTo(ChronoUnit.MINUTES);
+        model.addAttribute("time", time);
+
         model.addAttribute("donation", new Donation());
 
         return "form";
     }
 
     @PostMapping("/add")
-    public String formConfirmation () {
-        return "form-confirmation";
+    public String formConfirmation (@ModelAttribute Donation donation, BindingResult result) {
+
+        if (!result.hasErrors()) {
+            donationRepository.save(donation);
+            return "form-confirmation";
+        } else {
+            return "form";
+        }
     }
 }
