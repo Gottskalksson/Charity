ab9f602d62a94afbe24533aac20e28b671bea86b
Gottskalksson <adam.rolirad@gmail.com>
2020-07-06 18:59:53 +0200

    Added new classes to use services in Spring Security

--- src/main/java/pl/coderslab/charity/services/SpringDataUserDetailsService.java
+++ src/main/java/pl/coderslab/charity/services/SpringDataUserDetailsService.java
@@ -1,4 +1,26 @@
 package pl.coderslab.charity.services;
 
-public class SpringDataUserDetailsService {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pl.coderslab.charity.entity.User;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SpringDataUserDetailsService implements UserDetailsService {
+
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUserName(username);
+        if (user == null) {throw new UsernameNotFoundException(username); }
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        user.getRoles().forEach(r ->
+                grantedAuthorities.add(new SimpleGrantedAuthority(r.getName())));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(), user.getPassword(), grantedAuthorities);
+    }
 }
