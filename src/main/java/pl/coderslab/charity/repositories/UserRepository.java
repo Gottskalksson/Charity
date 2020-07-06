33c6a069abbe4cf19070cf129676c37bd75f7115
Gottskalksson <adam.rolirad@gmail.com>
2020-07-06 18:56:43 +0200

    Creater repo for new Entitis

--- src/main/java/pl/coderslab/charity/repositories/UserRepository.java
+++ src/main/java/pl/coderslab/charity/repositories/UserRepository.java
@@ -1,4 +1,12 @@
 package pl.coderslab.charity.repositories;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+
 }
