0150c9eb3a9e6826ccd371a5b381a8fbbfba4adc
Gottskalksson <adam.rolirad@gmail.com>
2020-07-23 21:39:48 +0200

    Create new controller

--- src/main/java/pl/coderslab/charity/controllers/AdminController.java
+++ src/main/java/pl/coderslab/charity/controllers/AdminController.java
@@ -1,8 +1,15 @@
 package pl.coderslab.charity.controllers;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.entity.User;
 import pl.coderslab.charity.repositories.UserRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/admin", produces = "text/plain;charset=utf-8")
@@ -12,5 +19,12 @@ public class AdminController {
 
     public AdminController(final UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String adminMenu(Model model) {
+        List<User> role_admin = userRepository.findAllByRoles(2);
+        model.addAttribute("admins", role_admin);
+        return "admin";
     }
 }
