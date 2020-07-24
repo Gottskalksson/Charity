4b6c8d2f84d988844418f679f9ada834838bb6a9
Gottskalksson <adam.rolirad@gmail.com>
2020-07-24 11:12:24 +0200

    Added controller to send admin's list

--- src/main/java/pl/coderslab/charity/controllers/AdminController.java
+++ src/main/java/pl/coderslab/charity/controllers/AdminController.java
@@ -1,14 +1,15 @@
 package pl.coderslab.charity.controllers;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pl.coderslab.charity.entity.Role;
 import pl.coderslab.charity.entity.User;
+import pl.coderslab.charity.repositories.RoleRepository;
 import pl.coderslab.charity.repositories.UserRepository;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -16,15 +17,18 @@ import java.util.List;
 public class AdminController {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public AdminController(final UserRepository userRepository) {
+    public AdminController(final UserRepository userRepository, final RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @GetMapping
     public String adminMenu(Model model) {
-        List<User> role_admin = userRepository.findAllByRoles(2);
-        model.addAttribute("admins", role_admin);
+        Role role_admin = roleRepository.findByName("ROLE_ADMIN");
+        List<User> usersByRoles = userRepository.findUsersByRoles(role_admin);
+        model.addAttribute("admins", usersByRoles);
         return "admin";
     }
 }
